76a8ad0b765ca3be2e1925bf7282cad59b318883
shansaybhatia <shansaybhatiaa@gmail.com>
2026-08-10 01:58:21 +0530

    filter map and reduce in js

---
+++ javascript/Lessons/05_Iterations/filter-map-reduce.js
@@ -0,0 +1,77 @@
+//Filter
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let newNums = myNums.filter((num) => {
+  return num > 4; // Always use return when using scope
+});
+
+//Using foreach
+// const newNums = [];
+// myNums.forEach((num) => {
+//   if (num > 4) {
+//     newNums.push(num);
+//   }
+// });
+
+console.log(newNums);
+
+const books = [
+  { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+  { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+  { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+  { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+  { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+  { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+  { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+  { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+];
+
+let userBooks = books.filter((bk) => bk.genre === "History");
+userBooks = books.filter((bk) => {
+  return bk.publish >= 1995 && bk.genre === "History";
+});
+console.log(userBooks);
+
+//Maps
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNumbers = myNumbers.map((num) => num + 10);
+
+//Chaining
+const newNumbers = myNumbers
+  .map((num) => num * 10)
+  .map((num) => num + 1)
+  .filter((num) => num >= 40);
+
+console.log(newNumbers);
+
+//Reduce  {(accumulator{uses initial value}, currentvalue), initialvalue}
+
+const myArr = [1, 2, 3];
+// const myTotal = myArr.reduce(function (acc, currval) {
+//   console.log(`acc: ${acc} and currval: ${currval}`);
+//   return acc + currval;
+// }, 0);
+
+//Arrow Function
+const myTotal = myArr.reduce((acc, currval) => acc + currval, 0);
+console.log(myTotal);
+
+const shoppingCart = [
+  {
+    item: "js Course",
+    price: 2999,
+  },
+  {
+    item: "Python",
+    price: 3999,
+  },
+  {
+    item: "Data Science",
+    price: 4999,
+  },
+];
+
+const cartTotal = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+console.log(cartTotal);
