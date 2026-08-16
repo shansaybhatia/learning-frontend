c98123a07fd05aaa17e002173625569f00b92662
shansaybhatia <shansaybhatiaa@gmail.com>
2026-08-16 23:02:44 +0530

    made bmi calculator and color theme changer

---
+++ javascript/projects/1-colorChanger/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    switch (event.target.id) {
+      case grey:
+        body.style.backgroundColor = event.target.id;
+        break;
+      case white:
+        body.style.backgroundColor = event.target.id;
+        break;
+      case blue:
+        body.style.backgroundColor = event.target.id;
+        break;
+      case yellow:
+        body.style.backgroundColor = event.target.id;
+        break;
+      default:
+        body.style.backgroundColor = event.target.id;
+        break;
+    }
+  });
+});
